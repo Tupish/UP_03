a70eadee08dce92569c36e24b926ba0d53774734
Tupish <timergalievip1@gmail.com>
2025-12-22 21:07:56 +0500

    Главная страница и подвал сайта

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,7 +1,17 @@
-<div class="container">
-    <p>☎️(34783)-5-00-76</p>
 
-    <p>🕒Пн-Пт 8:30-17:30</p>
-
-    <p>✉️post@nmknf.ru</p>
-</div>
+    <div class="container footer-container">
+        <div>
+            <h3>Контакты</h3>
+            <p>☎️ (34783)-5-00-76</p>
+            <p>🕒 Пн-Пт 8:30-17:30</p>
+            <p>✉️ post@nmknf.ru</p>
+        </div>
+        <div>
+            <h3>Мы в соцсетях</h3>
+            <div class="footer-links">
+                <a href="#">ВКонтакте</a>
+                <a href="#">Telegram</a>
+                <a href="#">YouTube</a>
+            </div>
+        </div>
+    </div>
